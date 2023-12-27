68d09102edf54d0b106ffb64ecaaae5adc20d03c
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2023-12-27 17:16:37 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -1,48 +1,54 @@
-import './App.css';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
 import * as d3 from "d3";
 
-
 function App() {
-
-  const [data, setData] = useState()
-  useEffect(()=> {
+  const [data, setData] = useState();
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json");
+        const response = await fetch(
+          "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json",
+        );
         const result = await response.json();
         setData(result);
       } catch (err) {
         console.log(err);
       }
     };
-    fetchData()
-  }, [])
-  
-  useEffect(()=> {
-    if(data) {
-      drawScatterPlot();
-      console.log('data', data);
-    }
-  }, [data])
+    fetchData();
+  }, []);
 
-function ApiRequestData() {
-  return (
-    <>
-    <div>{data? data.map((item, i) => <div key={i}>
-    <b>Doping:</b> {item.Doping} <br/> 
-    <b>Name:</b> {item.Name} <br/> 
-    <b>Place:</b> {item.Place} <br/> 
-    <b>Nationality:</b> {item.Nationality} <br/> 
-    <b>Seconds:</b> {item.Secons} <br/> 
-    <b>Time:</b> {item.Time} <br/> 
-    <b>Url:</b> {item.URL} <br/> 
-    <b>Year:</b> {item.Year} <br/> 
-    <br/> 
-    </div>) : null}</div>
-    </>
-  )
-}
+  useEffect(() => {
+    if (data) {
+      drawScatterPlot();
+      console.log("data", data);
+    }
+  }, [data]);
+
+  function ApiRequestData() {
+    return (
+      <>
+        <div>
+          {data
+            ? data.map((item, i) => (
+                <div key={i}>
+                  <b>Doping:</b> {item.Doping} <br />
+                  <b>Name:</b> {item.Name} <br />
+                  <b>Place:</b> {item.Place} <br />
+                  <b>Nationality:</b> {item.Nationality} <br />
+                  <b>Seconds:</b> {item.Secons} <br />
+                  <b>Time:</b> {item.Time} <br />
+                  <b>Url:</b> {item.URL} <br />
+                  <b>Year:</b> {item.Year} <br />
+                  <br />
+                </div>
+              ))
+            : null}
+        </div>
+      </>
+    );
+  }
 
   const noDopingColor = "rgb(255, 127, 14)";
   const DopingColor = "rgb(31, 119, 180)";
@@ -51,45 +57,54 @@ function ApiRequestData() {
   const padding = 40;
 
   const drawScatterPlot = () => {
+    d3.select(".App svg").remove();
 
-    d3.select(".App svg").remove()
-
-    const xScale = d3.scaleLinear()
+    const xScale = d3
+      .scaleLinear()
       // .domain([0 , data.length - 1])
-      .range([padding,  width - padding])
-    
-    const yScale = d3.scaleTime()
-      .range([padding, height - padding])
+      // l;akjdsf
 
-    const xAxis = d3.axisBottom(xScale)
-    const yAxis = d3.axisLeft(yScale)
+      .range([padding, width - padding]);
 
-    const svg = d3.select('#scatterContainer')
+    const yScale = d3.scaleTime().range([padding, height - padding]);
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    const svg = d3
+      .select("#scatterContainer")
       .append("svg")
-      .attr('width', width)
-      .attr('height', height)
-      .style('background-color', 'beige')
+      .attr("width", width)
+      .attr("height", height)
+      .style("background-color", "beige");
 
-      svg.selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-        .attr('cx', (d, i)=>  i * 10 ) // d.Year
-        .attr('cy', (d, i)=> 600 - parseInt(d.Time, 10) * 10) // d.Time
-        .attr('r', 4)
-        .attr('fill', (d)=> {return d.Doping? DopingColor : noDopingColor})
-        .attr('class', 'dot')
-  
-      svg.append('g')
-        .call(xAxis)
-        .attr('id', "x-axis")
-        .attr('transform', `translate(0, ${height - padding})`)
+    svg
+      .selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", (d, i) => i * 10) // d.Year
+      .attr("cy", (d, i) => 600 - parseInt(d.Time, 10) * 10) // d.Time
+      .attr("r", 4)
+      .attr("fill", (d) => {
+        return d.Doping ? DopingColor : noDopingColor;
+      })
+      .attr("class", "dot")
+      .attr("data-xvalue", (d) => d)
+      .attr("data-yvalue", (d) => d);
 
-      svg.append('g')
-        .call(yAxis)
-        .attr('id', 'y-axis')
-        .attr('transform', `translate(${padding}, 0)`)
-  }
+    svg
+      .append("g")
+      .call(xAxis)
+      .attr("id", "x-axis")
+      .attr("transform", `translate(0, ${height - padding})`);
+
+    svg
+      .append("g")
+      .call(yAxis)
+      .attr("id", "y-axis")
+      .attr("transform", `translate(${padding}, 0)`);
+  };
 
   return (
     <div className="App">
